615bd06aff62ee7937a8e8188b3d72c494f537b5
wanggang <wanggang@iboxpay.com>
2017-01-16 16:45:41 +0800

    修改代码

--- sunshine-web/src/main/java/com/sunshine/controller/UserController.java
+++ sunshine-web/src/main/java/com/sunshine/controller/UserController.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sunshine.service.UserService;
 
-@Controller("userController")
+@Controller
+@RequestMapping("user")
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
 	
-	@RequestMapping(value="/getUser")
+	@RequestMapping(value="/getUserName")
 	@ResponseBody
 	public String  getUser(){
 		return userService.getUserInfo().getUsername();
